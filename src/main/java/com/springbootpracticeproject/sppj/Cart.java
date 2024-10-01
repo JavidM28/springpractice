6aa8736b8e5b43bf6de7809f6016c3c6fed5c0d7
Javid <sairexm@gmail.com>
2024-10-01 20:19:50 +0400

    Final

--- src/main/java/com/springbootpracticeproject/sppj/Cart.java
+++ src/main/java/com/springbootpracticeproject/sppj/Cart.java
@@ -1,4 +1,24 @@
 package com.springbootpracticeproject.sppj;
 
+import java.util.Set;
+import java.util.HashSet;
+
 public class Cart {
+    private Set<Integer> itemIds;
+
+    public Cart() {
+        this.itemIds = new HashSet<>();  // Ensure the set is of type <Integer>
+    }
+
+    public void addItem(Integer itemId) {
+        itemIds.add(itemId);
+    }
+
+    public void addItems(Set<Integer> itemIds) {
+        this.itemIds.addAll(itemIds);
+    }
+
+    public Set<Integer> getItemIds() {
+        return itemIds;
+    }
 }
